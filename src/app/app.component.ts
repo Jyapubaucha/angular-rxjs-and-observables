e6ec34cb6cd2f67ddb8109814024957361fb4018
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-03-11 17:02:40 +0545

    Learned RxJS of() operator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,26 +11,31 @@ export class AppComponent {
 
   data: any[] = [];
 
+  array1 = [1,3,5,7,9];
+  array2 = ['A','B','C','D','E'];
 
   //1. Creating a observable
 
   //Observable
-  myObservable = new Observable((observer) => {
-    setTimeout(()=>{ observer.next(1); },1000);
+  // myObservable = new Observable((observer) => {
+  //   setTimeout(()=>{ observer.next(1); },1000);
 
-    setTimeout(()=>{ observer.next(2); },2000);
+  //   setTimeout(()=>{ observer.next(2); },2000);
 
-    setTimeout(()=>{ observer.next(3); },3000);
+  //   setTimeout(()=>{ observer.next(3); },3000);
 
-    // setTimeout(()=>{ observer.error(new Error('Something went wrong. Please try again later.')); },  3000);
+  //   // setTimeout(()=>{ observer.error(new Error('Something went wrong. Please try again later.')); },  3000);
 
-    setTimeout(()=>{ observer.next(4); },4000);
+  //   setTimeout(()=>{ observer.next(4); },4000);
 
-    setTimeout(()=>{ observer.next(5); },5000);
+  //   setTimeout(()=>{ observer.next(5); },5000);
 
-    setTimeout(()=>{ observer.complete(); },6000);
+  //   setTimeout(()=>{ observer.complete(); },6000);
 
-  });
+  // });
+
+
+  myObservable = of(this.array1, this.array2,20,30,'Deepak', true);
 
   GetAsyncData(){
       //Observer
