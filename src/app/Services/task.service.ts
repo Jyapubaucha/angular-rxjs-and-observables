e9bcb088d2b3aaa4847beee91840bd40089a1e0d
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-03-27 18:47:11 +0545

    Add and show task using subjects.

--- src/app/Services/task.service.ts
+++ src/app/Services/task.service.ts
@@ -1,12 +1,15 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class TaskService{
-    CreateTask: EventEmitter<string> = new EventEmitter<string>();
+    // CreateTask: EventEmitter<string> = new EventEmitter<string>();
+
+    CreateTask = new Subject<string>();
 
     OnCreateTask(value: string){
-        this.CreateTask.emit(value);
+        this.CreateTask.next(value);
     }
 }
