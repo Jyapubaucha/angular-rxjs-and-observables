650c3a241d96557da1d0cec5d1f4edbadb542184
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-03-27 19:14:16 +0545

    Fetch data from API using observable.

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import {ajax} from 'rxjs/ajax';
 
 @Component({
   selector: 'app-subject',
@@ -11,13 +12,23 @@ export class SubjectComponent implements OnInit {
   ngOnInit(){
     // let obs = new Observable((observer) => { observer.next(Math.random())}); 
 
-    const subject = new Subject();
+    // const subject = new Subject();
 
-    // Subscribe 1
-     subject.subscribe((data) => { console.log(data)});
-    // Subscribe 2
-    subject.subscribe((data) => { console.log(data)});
+    // // Subscribe 1
+    //  subject.subscribe((data) => { console.log(data)});
+    // // Subscribe 2
+    // subject.subscribe((data) => { console.log(data)});
 
-    subject.next(Math.random());
+    // subject.next(Math.random());
+
+  
+    //URL for random get method api "https://randomuser.me/api/"
+    //AJAX CALL
+
+    const data = ajax('https://randomuser.me/api/');
+    data.subscribe((res)=>{ console.log(res); });
+    data.subscribe((res)=>{ console.log(res); });
+    data.subscribe((res)=>{ console.log(res); });
+
   }
 }
